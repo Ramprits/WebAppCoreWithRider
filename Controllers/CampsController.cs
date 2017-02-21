0df4ad10669a1cdbd4dd9ac3d4e4177d64e3acc8
Ramprit Sahani <rampritsahani@outlook.in>
2017-02-21 14:21:02 +0530

    Hello this is my camps

---
+++ Controllers/CampsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebAppCore.Infrastructure;
+
+namespace WebAppCore.Controllers
+{
+    [Route("api/camps")]
+    public class CampsController : Controller
+    {
+        private readonly ILogger <CampsController> _logger;
+        private readonly ICampRepository _repo;
+
+        public CampsController(ILogger <CampsController> logger, ICampRepository repo)
+        {
+            _logger = logger;
+            _repo = repo;
+        }
+
+        public IActionResult Index() => View(_repo.GetAllCamps());
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                var camp = _repo.GetAllCamps();
+                if (camp == null) return BadRequest("This is bad requiest ");
+                return Ok(camp);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
